a3b1de5553032f29b8f966d31825124a41c01b4a
Mike Zborowski <mzborowski@yahoo.com>
2021-01-20 19:26:58 -0500

    1st step working - create/update s3 bucket website on codepush to github. Requires initialization steps, such as setting up IAM user and access keys. No bucket policy defined (unsafe). S3 bucket is public, which is ok for this demonstration; however, feature- and test- environments (bucket websites) would need to be restricted, lest the public accidentally visit one!

---
+++ terraform/develop/main.tf
@@ -0,0 +1,29 @@
+terraform {
+	required_providers {
+		aws	= {
+			source	= "hashicorp/aws"
+			version	= "~> 3.0"
+		}
+	}
+}
+
+provider "aws" {
+	region	= "us-east-1"
+	profile	= "r2g"
+}
+
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+#  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+}
+
+output "website_address" {
+	value	= aws_s3_bucket.site_bucket.website_endpoint
+}
